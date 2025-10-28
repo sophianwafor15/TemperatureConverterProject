9d79f3ee70b04a7bde412e8dcce7984f262a7207
Sophia Nwafor <sophianwafor15@gmail.com>
2025-10-27 17:29:44 -0700

    another commit

---
+++ src/test/java/TemperatureConverterTest.java
@@ -0,0 +1,14 @@
+import org.example.TemperatureConverter;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TemperatureConverterTest {
+
+    @Test
+    public void testTemperatureConverter() {
+        TemperatureConverter temperatureConverter = new TemperatureConverter();
+        assertEquals(41, 5);
+    }
+
+    public double
+}
