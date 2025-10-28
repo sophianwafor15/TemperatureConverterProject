6bf49dd28001c7804b4dcfabdbb06f63557d9f61
Sophia Nwafor <sophianwafor15@gmail.com>
2025-10-27 18:30:50 -0700

    steps completed

--- src/main/java/org/example/TemperatureConverter.java
+++ src/main/java/org/example/TemperatureConverter.java
@@ -1,17 +1,14 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class TemperatureConverter {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public double celsiusToFahrenheit(double celsius) {
+        return celsius * 1.8 + 32;
+    }
+    public double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) *  5 / 9;
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public double celsiusToKelvin(double celsius) {
+        return celsius + 273.15;
     }
 }
--- src/test/java/TemperatureConverterTest.java
+++ src/test/java/TemperatureConverterTest.java
@@ -7,8 +7,20 @@ public class TemperatureConverterTest {
     @Test
     public void testTemperatureConverter() {
         TemperatureConverter temperatureConverter = new TemperatureConverter();
-        assertEquals(41, 5);
+        assertEquals(41, temperatureConverter.celsiusToFahrenheit(5));
     }
 
-    public double
+    @Test
+    public void testTemperatureConverter2() {
+        TemperatureConverter temperatureConverter = new TemperatureConverter();
+        assertEquals(5,temperatureConverter.fahrenheitToCelsius(41));
+    }
+
+    @Test
+    public void testTemperatureConverter3() {
+        TemperatureConverter temperatureConverter = new TemperatureConverter();
+        assertEquals(373.15, temperatureConverter.celsiusToKelvin(100));
+
+
+    }
 }
